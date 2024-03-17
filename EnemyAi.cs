71e54d1e90b8a321c347fe043783069bf164852d
OsaBinawa <140168496+OsaBinawa@users.noreply.github.com>
2024-03-17 10:58:12 +0700

    improve

--- EnemyAi.cs
+++ EnemyAi.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    public int health = 100; // Example health value
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        // Handle other logic related to taking damage, e.g., checking for death
-    }
-
-}
+    [SerializeField] private int health = 100;
 
