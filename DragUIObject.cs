47e8da9e78a256181a25951f9dc8489ee7af5d29
OsaBinawa <140168496+OsaBinawa@users.noreply.github.com>
2024-03-17 10:58:19 +0700

    improve

--- DragUIObject.cs
+++ DragUIObject.cs
@@ -1,38 +1,38 @@
 using UnityEngine;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
 
-public class DragUIObject : MonoBehaviour, IDragHandler, IPointerDownHandler 
+public class DragUIObject : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     private RectTransform rectTransform;
     private Canvas canvas;
     private Vector2 originalLocalPointerPosition;
     private Vector3 originalPanelLocalPosition;
-    public float movementSensitivity = 1.0f; 
+    public float movementSensitivity = 1.0f;
 
     void Awake()
     {
-        rectTransform = GetComponent<RectTransform>(); 
-        canvas = GetComponentInParent<Canvas>(); 
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
     }
 
-    public void OnPointerDown(PointerEventData eventData) 
+    public void OnPointerDown(PointerEventData eventData)
     {
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition); //Using the event system to detect what is clicked on
-        originalPanelLocalPosition = rectTransform.localPosition;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.rectTransform, eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition))
+        {
+            originalPanelLocalPosition = rectTransform.localPosition;
+        }
     }
 
-    public void OnDrag(PointerEventData eventData) 
+    public void OnDrag(PointerEventData eventData)
     {
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out Vector2 localPointerPosition))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.rectTransform, eventData.position, eventData.pressEventCamera, out Vector2 localPointerPosition))
         {
             localPointerPosition /= canvas.scaleFactor;
 
-            
             Vector3 offsetToOriginal = (localPointerPosition - originalLocalPointerPosition) * movementSensitivity;
             rectTransform.localPosition = originalPanelLocalPosition + offsetToOriginal;
 
-           
-            Debug.Log($"Drag - LocalPointerPosition: {localPointerPosition}, Offset: {offsetToOriginal}, New Position: {rectTransform.localPosition}"); //Comment out this line if not debugging an issue, otherwise it will flood the console unnecessarily
+            Debug.Log($"Drag - LocalPointerPosition: {localPointerPosition}, Offset: {offsetToOriginal}, New Position: {rectTransform.localPosition}");
         }
     }
 }
